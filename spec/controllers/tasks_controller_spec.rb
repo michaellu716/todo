0e8769827148abcea21c7780353aaaf85e1a9a90
Michael Lu <michael_108@hotmail.com>
2019-07-30 23:28:36 +0000

    add tasks to the pg 17

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe TasksController, type: :controller do
 		end
 	end
 	describe "tasks#update" do
-    it "should allow tasks to be marked as done" do
+   		 it "should allow tasks to be marked as done" do
     		task = FactoryBot.create(:task, done: false)
     		put :update, params: {id: task.id, task: { done: true }}
     		expect(response).to have_http_status(:success)
@@ -25,4 +25,13 @@ RSpec.describe TasksController, type: :controller do
     		expect(task.done).to eq(true)
    		 end
   	end
+  	describe "tasks#create" do
+   		 it "should allow new tasks to be created" do
+    		post :create, params: {task: {title: "Fix things"}}
+      		expect(response).to have_http_status(:success)
+      		response_value = ActiveSupport::JSON.decode(@response.body)
+      		expect(response_value['title']).to eq("Fix things")
+      		expect(Task.last.title).to eq("Fix things")
+    end
+  end
 end
